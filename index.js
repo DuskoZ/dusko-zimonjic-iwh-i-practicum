8db10e1f29f8033a08c345677ca1e31a5108c1b8
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2025-02-02 15:28:08 +0100

    added update get route

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+require("dotenv").config();
+
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
+
+console.log(PRIVATE_APP_ACCESS);
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -17,6 +21,30 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+
+app.get("/update-cobj", async (req, res) => {
+    try {
+        const response = await axios.get(
+            "https://api.hubapi.com/crm/v3/objects/2-138650495/?properties=name,country,height,description",
+            {
+                headers: {
+                    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        const data = response.data.results.map((result) => result.properties);
+
+        console.log("Data:", data);
+
+        res.render("updates", {
+            title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+            data,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
@@ -66,6 +94,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
